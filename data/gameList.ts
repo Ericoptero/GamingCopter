355a5e86ff40524e5fdd265b3f54c166d220a929
Eric Nantes <ericnantes18@gmail.com>
2022-12-10 12:27:07 -0400

    Creating base data object

---
+++ data/gameList.ts
@@ -0,0 +1,53 @@
+type GameStatus = 'Não iniciado' | 'Jogando' | 'Não finalizado' | 'Joguei o suficiente' | 'Finalizado' | 'Desisti' | 'Lista de desejos';
+
+interface GameListData {
+  name: string;
+  platform: string;
+  status: GameStatus;
+  priority: 'Baixo' | 'Médio' | 'Alto' | null;
+  rating: number;
+  averageRating: number;
+  releaseDate: string;
+  dateAdded: string;
+  dateFinished: string;
+  notes: string;
+}
+
+export const data: GameListData[] = [
+  {
+    name: 'Rune Factory 4 Special',
+    platform: 'Nintendo Switch',
+    status: 'Finalizado',
+    priority: null,
+    rating: 4,
+    averageRating: 4.5,
+    releaseDate: '2020-12-12T00:00:00:000Z',
+    dateAdded: '2020-12-12T00:00:00:000Z',
+    dateFinished: '2020-12-12T00:00:00:000Z',
+    notes: 'Gostei muito, meu Rune Factory predileto, porém os gráficos apenas foram escalados, ainda parecem sprites das versões de DS.'
+  },
+  {
+    name: 'Rune Factory 4 Special',
+    platform: 'Nintendo Switch',
+    status: 'Finalizado',
+    priority: null,
+    rating: 4,
+    averageRating: 4.5,
+    releaseDate: '2020-12-12T00:00:00:000Z',
+    dateAdded: '2020-12-12T00:00:00:000Z',
+    dateFinished: '2020-12-12T00:00:00:000Z',
+    notes: 'Gostei muito, meu Rune Factory predileto, porém os gráficos apenas foram escalados, ainda parecem sprites das versões de DS.'
+  },
+  {
+    name: 'Rune Factory 4 Special',
+    platform: 'Nintendo Switch',
+    status: 'Finalizado',
+    priority: null,
+    rating: 4,
+    averageRating: 4.5,
+    releaseDate: '2020-12-12T00:00:00:000Z',
+    dateAdded: '2020-12-12T00:00:00:000Z',
+    dateFinished: '2020-12-12T00:00:00:000Z',
+    notes: 'Gostei muito, meu Rune Factory predileto, porém os gráficos apenas foram escalados, ainda parecem sprites das versões de DS.'
+  }
+]
